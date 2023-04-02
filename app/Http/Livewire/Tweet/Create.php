8be320a9a79beb6ff232f7289a4f8cc19da13117
Daniel Felipe <daniell.felipe.2017@gmail.com>
2023-04-02 12:05:58 -0300

    Be able to create a new tweet

---
+++ app/Http/Livewire/Tweet/Create.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire\Tweet;
+
+use App\Models\Tweet;
+use Livewire\Component;
+use Illuminate\Contracts\View\View;
+
+class Create extends Component
+{
+    public ?string $body = null;
+
+    public function render(): View
+    {
+        return view('livewire.tweet.create');
+    }
+
+    public function tweet(): void
+    {
+        Tweet::query()->create([
+            'body' => $this->body,
+            'created_by' => auth()->id(),
+        ]);
+        
+        $this->emit('tweet::created');
+    }
+}
--- tests/Feature/TweetTest.php
+++ tests/Feature/TweetTest.php
@@ -1,6 +1,31 @@
 <?php
 
-todo('should be able to create a tweet');
+use App\Models\User;
+use App\Models\Tweet;
+use App\Http\Livewire\Tweet\Create;
+use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
+use function Pest\Laravel\assertDatabaseCount;
+
+it('should be able to create a tweet', function () {
+    $user = User::factory()->create();
+
+    \Pest\Laravel\actingAs($user);
+
+    livewire(Create::class)
+        ->set('body', 'This is my first tweet')
+        ->call('tweet')
+        ->assertEmitted('tweet::created');
+
+    assertDatabaseCount('tweets', 1);
+
+    expect(Tweet::first())
+        ->body->toBe('This is my first tweet')
+        
+        ->created_by->toBe($user->id);
+});
+
+todo('should make sure that only authenticated user users can tweet');
 todo('body is required');
 todo('the tweet body should have a max length of 140 characters');
 todo('sould show the tweet on the timeline');
